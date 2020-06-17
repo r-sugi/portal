2fff4e1c3413ac86a226c7b9e8469a89859a5d04
r-sugi <r.sugimoto0615@gmail.com>
2020-06-18 01:55:14 +0900

    update: config of meta

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,4 +1,8 @@
 module.exports = {
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+  ],
   head: [
     ['link', { rel: 'icon', type: 'image/jpg', href: '/favicon.jpg' }],
   ],
